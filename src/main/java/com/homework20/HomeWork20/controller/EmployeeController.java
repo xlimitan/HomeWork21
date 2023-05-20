6b0eacce76273a92268623e5a8eef0e380177e16
xlimitan <xlimitan@yandex.ru>
2023-05-20 16:15:40 +0300

    DZ

--- src/main/java/com/homework20/HomeWork20/controller/EmployeeController.java
+++ src/main/java/com/homework20/HomeWork20/controller/EmployeeController.java
@@ -25,8 +25,8 @@ public class EmployeeController {
     }
 
     @GetMapping("/add")
-        public Employee add(String firstName, String lastName) {
-        return employeeService.add(new Employee(firstName, lastName));
+        public Employee add(String firstName, String lastName, Integer department, double salary) {
+        return employeeService.add(new Employee(firstName, lastName, department, salary));
     }
     @GetMapping("/remove")
     public Employee remove(String firstName, String lastName) {
