373ecfc03ae3913dc4c3db7de76d0308a5140575
xlimitan <xlimitan@yandex.ru>
2023-05-20 15:31:36 +0300

    Initial commit

---
+++ src/main/java/com/homework20/HomeWork20/HomeWork20Application.java
@@ -0,0 +1,13 @@
+package com.homework20.HomeWork20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework20/HomeWork20/HomeWork20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework20.HomeWork20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
